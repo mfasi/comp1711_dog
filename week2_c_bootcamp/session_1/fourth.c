ff14b637f6f90f6d99886d14b0a1e17fe712df4d
Massimiliano Fasi <massimiliano.fasi@gmail.com>
2023-10-18 10:53:44 +0000

    Delete removed files

--- week2_c_bootcamp/session_1/fourth.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main () {
-    char name [10];
-    printf("Insert a name: ");
-    scanf("%s", name);
-    printf("Hello, %s\n", name);
-    return 0;
-}
--- week2_c_bootcamp/session_1/third.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main () {
-    float var;
-    printf("Insert a number: ");
-    scanf("%f", &var);
-    printf("var = %f\n", var);
-    return 0;
-}
